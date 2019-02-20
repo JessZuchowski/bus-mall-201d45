a19121539932e32a5eeb68c0bb135462da21dd24
jaezuchowski <jess.zuchowski@gmail.com>
2019-02-19 17:36:03 -0800

    redo for first commit

--- js/app.js
+++ js/app.js
@@ -1,128 +1,94 @@
 'use strict';
 
 //Global variables
-var total_items = 20;
+//var NUMBER_OF_CLICKS is static, the_catalog will be an array. Creating an item_container for the main body of the page. Created items at three positions by item Id. Declares vars for displayed_items.
+var NUMBER_OF_CLICKS = 25;
 var the_catalog = [];
 
-var currently_displayed_left_item;
-var currently_displayed_center_item;
-var currently_displayed_right_item;
+var item_container = document.body;
 
-var catalog_container = document.getElementById('all_items');
+var item_left = document.getElementById('item_left');
+var item_center = document.getElementById('item_center');
+var item_right = document.getElementById('item_right');
 
-var left_h2 = document.getElementById('left_item_h2');
-var center_h2 = document.getElementById('center_item_h2');
-var right_h2 = document.getElementById('right_item_h2');
-
-var left_img = document.getElementById('left_item_img');
-var center_img = document.getElementById('center_item_img');
-var right_img = document.getElementById('right_item_img');
-
-//Product Constructor: includes item, url, and click count.
+//Item Constructor: includes item name, url, click count, and appeared. Pushes to the_catalog.
 var Item = function(name, url){
   this.name = name;
   this.url = url;
-  this.clicked_on_count = 0;
+  this.clicks = 0;
+  this.appeared = 0;
 
-  the_catalog.push(this);
+  the_catalog.push(this); 
+};
+Item.prototype.render_as_image = function(target_img){
+  target_img.src = this.url;
 };
 
-//Image renderer: finds target, creates new element, gives content, appends to page.
-var render_item = function(item, target_img, target_h2){
-  target_img.src = item.url;
-  target_h2.textContent = item.name;
-};
+new Item('Bag', './img/bag.jpg');
+new Item('Banana', './img/banana.jpg');
+new Item('Bathroom', './img/bathroom.jpg');
+new Item('Boots', './img/boots.jpg');
+new Item('Breakfast', './img/breakfast.jpg');
+new Item('Bubblegum', './img/bubblegum.jpg');
+new Item('Chair', './img/chair.jpg');
+new Item('Cthulhu', './img/cthulhu.jpg');
+new Item('Dog-Duck', './img/dog-duck.jpg');
+new Item('Dragon', './img/dragon.jpg');
+new Item('Pen', './img/pen.jpg');
+new Item('Pet-Sweep', './img/pet-sweep.jpg');
+new Item('Scissors', './img/scissors.jpg');
+new Item('Shark', './img/shark.jpg');
+new Item('Sweep', './img/sweep.png');
+new Item('Tauntaun', './img/tauntaun.jpg');
+new Item('Unicorn', './img/unicorn.jpg');
+new Item('USB', './img/usb.gif');
+new Item('Water-Can', './img/water-can.jpg');
+new Item('Wine-Glass', './img/wine-glass.jpg');
 
-//Event Handler: tallies votes and total clicks, shows new items. 
+var displayed_item_left = the_catalog[0];
+var displayed_item_center = the_catalog[1];
+var displayed_item_right = the_catalog[2];
 
-var handle_click_on_item = function(event){
-  if(event.target.tagName === 'IMG'){
-    if(event.target.id === 'left_item_img'){
-      currently_displayed_left_item.clicked_on_count ++;    
-    }else if (event.target.id === 'center_item_img'){
-      currently_displayed_center_item.clicked_on_count ++;   
-    }else if (event.target.id === 'right_item_img'){
-      currently_displayed_right_item.clicked_on_count ++;
-    }
-    total_items--; 
 
-//Pick new items.
-
-    var left_item_idx = Math.floor(Math.random() * the_catalog.length);
-    var center_item_idx = Math.floor(Math.random() * the_catalog.length);
-    var right_item_idx = Math.floor(Math.random() * the_catalog.length);
-
-//Update currently displayed items.
-
-    currently_displayed_left_item = the_catalog[left_item_idx];
-    currently_displayed_center_item = the_catalog[center_item_idx];
-    currently_displayed_right_item = the_catalog[right_item_idx];
-
-//Call render function for 3 new items.
-
-    render_item(the_catalog[left_item_idx], left_img, left_h2);
-    render_item(the_catalog[center_item_idx], center_img, center_h2);
-    render_item(the_catalog[right_item_idx], right_img, right_h2);
-
-//To stop listening
-    if(total_items <= 0){
-      catalog_container.removeEventListener('click', handle_click_on_item);
-    }
+//Event Handler: tallies votes and total clicks.
+function handle_item_click (event) {
+  NUMBER_OF_CLICKS--; //decrements clicks
+  if(event.target.id === 'item_left'){
+    displayed_item_left.clicks++;
   }
-}; 
+  if(event.target.id === 'item_center'){
+    displayed_item_center.clicks++; 
 
-//Init Area: create all items.
+  }
+  if(event.target.id === 'item_right'){
+    displayed_item_right.clicks++;
 
-var bag_item = new Item('Bag', './img.bag.jpg');
-var banana_item = new Item('Banana', './img.banana.jpg');
-var bathroom_item = new Item('Bathroom', './img.bathroom.jpg');
-var boots_item = new Item('Boots', './img.boots.jpg');
-var breakfast_item = new Item('Breakfast', './img.breakfast.jpg');
-var bubblegum_item = new Item('Bubblegum', './img.bubblegum.jpg');
-var chair_item = new Item('Chair', './img.chair.jpg');
-var cthulhu_item = new Item('Cthulhu', './img.cthulhu.jpg');
-var dogduck_item = new Item('Dog-Duck', './img.dog-duck.jpg');
-var dragon_item = new Item('Dragon', './img.dragon.jpg');
-var pen_item = new Item('Pen', './img.pen.jpg');
-var petsweep_item = new Item('Pet-Sweep', './img.pet-sweep.jpg');
-var scissors_item = new Item('Scissors', './img.scissors.jpg');
-var shark_item = new Item('Shark', './img.shark.jpg');
-var sweep_item = new Item('Sweep', './img.sweep.jpg');
-var tauntaun_item = new Item('Tauntaun', './img.tauntaun.jpg');
-var unicorn_item = new Item('Unicorn', './img.unicorn.jpg');
-var usb_item = new Item('USB', './img.usb.jpg');
-var watercan_item = new Item('Water-Can', './img.water-can.jpg');
-var wineglass_item = new Item('Wine-Glass', './img.wine-glass.jpg');
+  }
 
-new Item('Bag', './img.bag.jpg');
-new Item('Banana', './img.banana.jpg');
-new Item('Bathroom', './img.bathroom.jpg');
-new Item('Boots', './img.boots.jpg');
-new Item('Breakfast', './img.breakfast.jpg');
-new Item('Bubblegum', './img.bubblegum.jpg');
-new Item('Chair', './img.chair.jpg');
-new Item('Cthulhu', './img.cthulhu.jpg');
-new Item('Dog-Duck', './img.dog-duck.jpg');
-new Item('Dragon', './img.dragon.jpg');
-new Item('Pen', './img.pen.jpg');
-new Item('Pet-Sweep', './img.pet-sweep.jpg');
-new Item('Scissors', './img.scissors.jpg');
-new Item('Shark', './img.shark.jpg');
-new Item('Sweep', './img.sweep.jpg');
-new Item('Tauntaun', './img.tauntaun.jpg');
-new Item('Unicorn', './img.unicorn.jpg');
-new Item('USB', './img.usb.jpg');
-new Item('Water-Can', './img.water-can.jpg');
-new Item('Wine-Glass', './img.wine-glass.jpg');
+  console.log('clicked');
 
-currently_displayed_left_item = the_catalog[0];
-currently_displayed_center_item = the_catalog[1];
-currently_displayed_right_item = the_catalog[3];
+  if (NUMBER_OF_CLICKS <= 0) {
+    item_container.removeEventListener('click', handle_item_click);
+  }
+  
 
-catalog_container.addEventListener('click', handle_click_on_item);
+//Pick new items using random integers times the length of the_catalog.
+  var random_left = Math.floor(Math.random() * the_catalog.length);
+  var random_center = Math.floor(Math.random() * the_catalog.length);
+  var random_right = Math.floor(Math.random() * the_catalog.length);
+//Update currently displayed items. Tied to the_catalog array, not images. 
+  the_catalog[random_left].render_as_image(item_left);
+  the_catalog[random_center].render_as_image(item_center);
+  the_catalog[random_right].render_as_image(item_right);
 
-function named_function () {
-  console.log();
+//Call render function for 3 new items using catalog index and ids. 
+  displayed_item_left = the_catalog[random_left];
+  displayed_item_center = the_catalog[random_center];
+  displayed_item_right = the_catalog[random_right];
 }
-console.log(handle_click_on_item);
-console.log(named_function);
+
+item_container.addEventListener('click', handle_item_click);
+
+//Init Area: put three items on page, listen for clicks, create all items.
+
+
